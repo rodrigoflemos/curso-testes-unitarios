c1e2180b8398d6d2546974e0e7cd7f06bb294138
Rodrigo Lemos <rodrigoflemos@gmail.com>
2019-01-29 14:29:46 -0200

    Primeiro Commit

---
+++ src/main/java/br/com/lemos/testesunitarios/CursoTestesUnitariosApplication.java
@@ -0,0 +1,14 @@
+package br.com.lemos.testesunitarios;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CursoTestesUnitariosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CursoTestesUnitariosApplication.class, args);
+	}
+
+}
+
